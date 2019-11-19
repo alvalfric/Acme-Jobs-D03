cab8104b9269f41b4aaa60f0a19a3f6841ba8ce1
José Manuel Escobar <josescrue@alum.us.es>
2019-11-19 17:22:47 +0100

    Task-019-josescrue: Finished

--- src/main/java/acme/entities/offers/Offer.java
+++ src/main/java/acme/entities/offers/Offer.java
@@ -25,7 +25,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(indexes = {
-	@Index(columnList = "id"), @Index(columnList = "ticker")
+	@Index(columnList = "id, deadline"), @Index(columnList = "ticker, deadline"), @Index(columnList = "deadline")
 })
 public class Offer extends DomainEntity {
 
